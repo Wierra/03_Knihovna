dcb13cb9e07dce811841ddc31ea420b4ef7bc6a1
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-07 20:31:08 +0200

    Řešení příkladu

---
+++ script.js
@@ -0,0 +1,22 @@
+const kniha1 = {
+    nazev: "Konec prokrastinace",
+    autor: "Petr Ludwig",
+    cena: 350,
+    popis: "Jak přestat odkládat a začít žít každý den naplno.",
+    jazyk: "čeština",
+    pocetStran: 250,
+    vydani: {
+        rok: 2019,
+        mesic: 12,
+    },
+    vazba: "tištěná",
+    rozmer:{
+        sirka: 120,
+        vyska: 127,
+        rozJednotka: "mm",
+    },
+    hmotnost: 65,
+    hmJednotka: "g",
+}
+
+const kniha2 = {}
